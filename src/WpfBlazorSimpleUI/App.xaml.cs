1d686bf3c3c0b6601f89613ec1b2ac03eb954193
Sysinfocus <info@sysinfocus.com>
2025-03-25 11:22:29 +0530

    Initial setup

---
+++ src/WpfBlazorSimpleUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfBlazorSimpleUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
